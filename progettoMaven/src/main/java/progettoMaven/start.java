7bd5255d1aaf0ee7f662307a26144bbb0a2d4722
davide.rapisarda <davide.rapisarda@host.docker.internal>
2023-12-18 11:34:04 +0100

    primo commit

---
+++ progettoMaven/src/main/java/progettoMaven/start.java
@@ -0,0 +1,10 @@
+package progettoMaven;
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.pssp("Ciao mondo il vero terrone sono io");
+	}
+
+}
